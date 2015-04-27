fa39488d9201a9d2858c91d60922b83eb7bdd1d4
Igor <krasilich@igor.krasilich>
2015-04-27 19:58:40 +0300

    Add Navigation and Google map

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! =)'
+def google_map():
+    return render_template('map.html')
+
+@app.route('/demand')
+def demand():
+    return render_template('demand.html')
+
+@app.route('/ads')
+def ads():
+    return render_template('ads.html')
+
+@app.route('/apps')
+def apps():
+    return render_template('apps.html')
+
+@app.route('/extra')
+def extra():
+    return render_template('extra.html')
 
 if __name__ == '__main__':
     app.debug = True
